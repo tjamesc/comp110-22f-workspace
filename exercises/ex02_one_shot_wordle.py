7ec1e1b58d6cc3386d7732d6e1a670f913aecb62
Thomas Carriero <tcarriero@unc.edu>
2022-09-06 11:02:00 -0400

    exercise 2 w/ comments

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -23,15 +23,15 @@ while playing:
         print(box_sequence)
         print("Woo! You got it!")
         playing = False
-        #ends the game and displays all green boxes if the user guesses correctly
+#ends the game and displays all green boxes if the user guesses correctly
     elif len(guess) != len(SECRET):
         guess = input(f"That was not {len(SECRET)} letters! Try again: ")
-        #if the guess has an incorrect length, the user will be prompted for a new input until a valid guess is given
+#if the guess has an incorrect length, the user will be prompted for a new input until a valid guess is given
     else:
         while i + 1 <= len(SECRET):
             if guess[i] == SECRET[i]:
                 box_sequence += GREEN_BOX
-                #if the guess is valid but incorrect, we enter this if-statement, giving green boxes for correct letter guesses
+#if the guess is valid but incorrect, we enter this if-statement, giving green boxes for correct letter guesses
             else:
                 letter_present: bool = False
                 j: int = 0
@@ -46,11 +46,11 @@ while playing:
                     box_sequence += YELLOW_BOX
                 else:
                     box_sequence += WHITE_BOX
-                    #the else block of the aforementioned if-statement contains the code to determine whether or
-                    #ot a letter is present in the word, even if it's in the wrong spot, using a comparison search, where
-                    #the index of the secret is changed and the guessed letter index is constant until all spots are checked
+#the else block of the aforementioned if-statement contains the code to determine whether or
+#not a letter is present in the word, even if it's in the wrong spot, using a comparison search, where
+#the index of the secret is changed and the guessed letter index is constant until all spots are checked
             i += 1
         print(box_sequence)
         print("Not quite. Play again soon!")
         playing = False
-        #the playing = False statements ensure the while loop stops running when the game is over
+#the playing = False statements ensure the while loop stops running when the game is over
