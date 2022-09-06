deb33110361d9d2066f739e3720f1039985733f9
Thomas Carriero <tcarriero@unc.edu>
2022-09-06 00:19:22 -0400

    exercise 2 completed

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,6 +1,6 @@
 """One-shot wordle exercise for COMP 110."""
 
-__author__ = 730510525
+__author__ = "730510525"
 
 
 SECRET: str = "python"
@@ -17,14 +17,14 @@ i: int = 0
 box_sequence: str = ""
 
 
-while playing == True:
+while playing:
     if guess == SECRET:
-        box_sequence: str = GREEN_BOX * len(SECRET)
+        box_sequence = GREEN_BOX * len(SECRET)
         print(box_sequence)
         print("Woo! You got it!")
-        playing: bool = False
+        playing = False
     elif len(guess) != len(SECRET):
-        guess: str = input(f"That was not {len(SECRET)} letters! Try again: ")
+        guess = input(f"That was not {len(SECRET)} letters! Try again: ")
     else:
         while i + 1 <= len(SECRET):
             if guess[i] == SECRET[i]:
@@ -33,17 +33,17 @@ while playing == True:
                 letter_present: bool = False
                 j: int = 0
                 search: bool = True
-                while j + 1 <= len(SECRET) and search == True:
+                while j + 1 <= len(SECRET) and search:
                     if guess[i] == SECRET[j]:
-                        search: bool = False
-                        letter_present =  True
+                        search = False
+                        letter_present = True
                     else:
                         j += 1
-                if letter_present == True:
+                if letter_present:
                     box_sequence += YELLOW_BOX
                 else:
                     box_sequence += WHITE_BOX
             i += 1
         print(box_sequence)
         print("Not quite. Play again soon!")
-        playing: bool = False
+        playing = False
